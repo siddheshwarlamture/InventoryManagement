dc75687f6a2aa3e831488a252d63d27ac83e89b8
Ganesh Swami <swamivaishnavi347@gmail.com>
2023-11-20 08:40:53 +0530

    Update models

---
+++ InventoryManagement/Models/CompanyViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace InventoryManagement.Models
+{
+    public class CompanyViewModel
+    {
+        public int Id { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string transportName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string EmailId { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string GstNo { get; set; }
+
+    }
+}
